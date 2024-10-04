e04b2a77b5e245dc3d9dd5ea4d87c26bd81d460a
ajmeno001 <34585853+ajmeno001@users.noreply.github.com>
2024-10-03 19:57:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,15 @@ def load_data():
 existing_data = load_data()
 
 def send_confirmation_email(email, first_name, pet_type, pet_breed):
-    # Use Streamlit secrets for email credential
-sender_email = os.environ.get("SENDER_EMAIL", "menofinance2022@outlook.com")
-sender_password = os.environ.get("SENDER_PASSWORD", "USDcad23!!")
+    # Use Streamlit secrets for email credentials
+    sender_email = st.secrets["email"]["SENDER_EMAIL"]
+    sender_password = st.secrets["email"]["SENDER_PASSWORD"]
+    
     print("Available secret keys:", st.secrets.keys())
-if "email" in st.secrets:
-    print("Email secret keys:", st.secrets["email"].keys())
-else:
-    print("Email secrets not found")
+    if "email" in st.secrets:
+        print("Email secret keys:", st.secrets["email"].keys())
+    else:
+        print("Email secrets not found")
     
     if not sender_email or not sender_password:
         st.error("Sender email or password not set in Streamlit secrets.")
